1a5128e7c394625982f84e0331eca0e1803f1f69
tommyallen <thomasclansi228@gmail.com>
2024-09-25 19:14:18 +0700

    Start to write operations MainActivity

--- app/src/main/java/com/example/lab4/MainActivity.kt
+++ app/src/main/java/com/example/lab4/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.lab4
 
+
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,15 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener { view: View ->
+// Что-то выполнить после нажатия
+        }
+        falseButton.setOnClickListener { view: View ->
+// Что-то выполнить после нажатия
+        }
+
     }
 }
