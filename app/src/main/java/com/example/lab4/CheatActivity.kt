34b41205c07a18f472cf34df65c7c39eae86e139
tommyallen <thomasclansi228@gmail.com>
2024-10-04 20:12:40 +0700

    editing AndroidManifest.xml

--- app/src/main/java/com/example/lab4/CheatActivity.kt
+++ app/src/main/java/com/example/lab4/CheatActivity.kt
@@ -3,45 +3,45 @@ package com.example.lab4
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.Button
-import android.widget.TextView
 
-private const val EXTRA_ANSWER_IS_TRUE="com.bignerdranch.android.lab4.answer_is_true"
+private const val EXTRA_ANSWER_IS_TRUE="com.bignerdranch.android.lab_4.answer_is_true"
+
 class CheatActivity : AppCompatActivity() {
-    private lateinit var answerTextView:TextView
-    private lateinit var showAnswerButton:Button
-    private var answerIsTrue=false
-
+    private lateinit var answerTextView: TextView
+    private lateinit var showAnswerButton: Button
+    private var answerIsTrue = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_cheat)
-        answerIsTrue=intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE,false)
-        answerTextView=findViewById(R.id.answer_text_view)
-        showAnswerButton=findViewById(R.id.show_answer_button)
+        answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
+        answerTextView = findViewById(R.id.answer_text_view)
+        showAnswerButton = findViewById(R.id.show_answer_button)
         showAnswerButton.setOnClickListener()
         {
-            val answerText=when{
-                answerIsTrue->R.string.true_button
-                else-> R.string.false_button
+            val answerText = when {
+                answerIsTrue -> R.string.true_button
+                else -> R.string.false_button
             }
             answerTextView.setText(answerText)
         }
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
-    companion object{
-        fun newIntent(packageContext: Context,answerIsTrue:Boolean): Intent {
-            return Intent(packageContext,CheatActivity::class.java).apply {
-                putExtra(EXTRA_ANSWER_IS_TRUE,answerIsTrue)
+
+    companion object {
+        fun newIntent(packageContext: Context, answerIsTrue: Boolean): Intent {
+            return Intent(packageContext, CheatActivity::class.java).apply {
+                putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue)
             }
         }
     }
